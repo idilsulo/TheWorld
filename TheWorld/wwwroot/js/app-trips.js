afb66e092c88b19d910533817769b1cfc38837c5
Idil Sulo <idilsulo@outlook.com>
2017-08-16 15:42:44 +0300

    Two JavaScript files named app-trips.js and tripsController.js are added

---
+++ TheWorld/wwwroot/js/app-trips.js
@@ -0,0 +1,10 @@
+﻿//app-trips.js
+
+(function () {
+
+    "use strict";
+
+    /* Creating the module */
+    angular.module("app-trips", []);
+
+})();
---
+++ TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,18 @@
+﻿//tripsController.js
+
+(function () {
+
+    "use strict";
+
+    /* Getting the existing module */
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+    function tripsController() {
+
+        var vm = this;
+        vm.name = "Shawn";
+    }
+
+
+})();
