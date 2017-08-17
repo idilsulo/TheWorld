9ef8622452b4ad8bd7f16decab464b33d8dd58fd
Idil Sulo <idilsulo@outlook.com>
2017-08-17 17:12:23 +0300

    Table still does not get the two trip objects named US Trip and WorldTrip

--- TheWorld/wwwroot/js/tripsController.js
+++ TheWorld/wwwroot/js/tripsController.js
@@ -1,5 +1,4 @@
 ﻿//tripsController.js
-
 (function () {
 
     "use strict";
@@ -11,22 +10,55 @@
     function tripsController($http) {
 
         var vm = this;
-
-        vm.trips = []; 
+        /*vm.trips = [{
+            name: "US Trip",
+            created: new Date()
+        }, {
+                name: "World Trip",
+                created: new Date()
+            }]; */
+        vm.trips = [];
 
         vm.newTrip = {};
 
+        vm.errorMessage = "";
+
+        vm.isBusy = true;
+
         $http.get("/api/trips")
             .then(function (response) {
-                // Success
+                // Success 
                 angular.copy(response.data, vm.trips);
-            }, function () {
-                //Failure
+            }, function (error) {
+                // Failure 
+                vm.errorMessage = "Failed to load data" + error;
+            })
+            .finally(function () {
+                vm.isBusy = false;
             });
 
-        vm.AddTrip = function () {
-            vm.trips.push({ name: vm.newTrip.name, created: new Date() });
-            vm.newTrip = {};
+
+        vm.addTrip = function () {
+            //vm.trips.push({ name: vm.newTrip.name, created: new Date() });
+            //vm.newTrip = {}; // Clears the form to prevent the user from accidentally adding the same trip over and over again 
+
+
+            vm.isBusy = true;
+            vm.errorMessage = "";
+
+            $http.post("/api/trips", vm.newTrip)
+                .then(function (response) {
+                    // Success
+                    vm.trips.push(response.data);
+                    vm.newTrip = {};
+                }, function () {
+                    // Failure 
+                    vm.errorMessage = "Failed to save new trip";
+                })
+                .finally(function () {
+                    vm.isBusy = false;
+                });
+
         };
     }
 
