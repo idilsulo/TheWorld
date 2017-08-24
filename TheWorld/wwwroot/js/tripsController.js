5002145b84f248033eaae6f644fc0b21d0f79d02
Idil Sulo <idilsulo@outlook.com>
2017-08-24 14:02:02 +0300

    Changed

--- TheWorld/wwwroot/js/tripsController.js
+++ TheWorld/wwwroot/js/tripsController.js
@@ -1,58 +1,47 @@
-﻿//tripsController.js
+﻿// tripsController.js
 (function () {
 
     "use strict";
 
-    /* Getting the existing module */
+    // Getting the existing module
     angular.module("app-trips")
         .controller("tripsController", tripsController);
 
     function tripsController($http) {
 
         var vm = this;
-        /*vm.trips = [{
-            name: "US Trip",
-            created: new Date()
-        }, {
-                name: "World Trip",
-                created: new Date()
-            }]; */
+
         vm.trips = [];
 
         vm.newTrip = {};
 
         vm.errorMessage = "";
-
         vm.isBusy = true;
 
         $http.get("/api/trips")
             .then(function (response) {
-                // Success 
+                // Success
                 angular.copy(response.data, vm.trips);
             }, function (error) {
-                // Failure 
-                vm.errorMessage = "Failed to load data" + error;
+                // Failure
+                vm.errorMessage = "Failed to load data: " + error;
             })
             .finally(function () {
                 vm.isBusy = false;
             });
 
-
         vm.addTrip = function () {
-            //vm.trips.push({ name: vm.newTrip.name, created: new Date() });
-            //vm.newTrip = {}; // Clears the form to prevent the user from accidentally adding the same trip over and over again 
-
 
             vm.isBusy = true;
             vm.errorMessage = "";
 
             $http.post("/api/trips", vm.newTrip)
                 .then(function (response) {
-                    // Success
+                    // success
                     vm.trips.push(response.data);
                     vm.newTrip = {};
                 }, function () {
-                    // Failure 
+                    // failure
                     vm.errorMessage = "Failed to save new trip";
                 })
                 .finally(function () {
@@ -60,7 +49,9 @@
                 });
 
         };
-    }
 
+
+
+    }
 
 })();
